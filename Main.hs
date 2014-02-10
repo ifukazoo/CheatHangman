9cb30160dc16bd9b855e63cdbc1bdfad8c23db49
yamanobori_old <yamanobori_old@yahoo.co.jp>
2014-02-10 21:57:34 +0900

    mainループができた

--- Main.hs
+++ Main.hs
@@ -1,13 +1,60 @@
 module Main (main) where
 
 import System.Environment(getArgs)
-
+import System.Exit
 import Hangman
+
 
 main = do
     args <- getArgs
-    let (filePath:((letter:_):_)) = args
-    wordList <- readWordFile filePath
-    displayList $ map f (patternByLetter (removeWordsWithoutLetter letter wordList) letter)
-                        where
-                             f ints = show ints
+    wordList <- readFile $ head args
+    let targetWord = "*******"
+    putStrLn targetWord
+    mainLoop targetWord $ lines wordList
+        where
+            mainLoop:: String -> [String] -> IO ()
+            mainLoop targetWord wordList = do
+                (letter:_) <- getLine
+                let (newWordList, revealedWord) = cheatHangMan wordList targetWord letter
+                putStrLn revealedWord
+                if all (/= '*') revealedWord
+                then putStrLn "success"
+                else putStrLn "failure" >> mainLoop targetWord newWordList
+
+            cheatHangMan :: [String] -> String -> Char -> ([String], String)
+            cheatHangMan _ _ letter = (["ki"], "le*ter")
+
+
+    -- wordList <- mapM f (lines contents)
+    -- mapM_ (putStrLn.snd) wordList
+    --     where
+    --         f :: String -> IO ([String], String)
+    --         f (letter:_) = return (["ki"], "@@@@@")
+
+                
+--
+--
+
+-- TEST
+-- main = do
+--     args <- getArgs
+--     let (filePath:((letter:_):_)) = args
+--     wordList <- readWordFile filePath
+--     let p = mostFreqPatternByLetter  wordList  letter
+--     putStrLn $ show p
+
+-- TEST
+-- main = do
+--     args <- getArgs
+--     let (filePath:((letter:_):_)) = args
+--     wordList <- readWordFile filePath
+--     putStrLn $ show $ patternByLetter wordList letter
+                                 --
+-- TEST
+-- main = do
+--     args <- getArgs
+--     let (filePath:((letter:_):_)) = args
+--     wordList <- readWordFile filePath
+--     let (p, _) = mostFreqPatternByLetter  wordList  letter
+--     mapM_ putStrLn $ map show p
+--
